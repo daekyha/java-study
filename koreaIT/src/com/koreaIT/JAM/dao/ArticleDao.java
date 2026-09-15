c9c8ccc508df54fc965e57805e0536ed0efb5eef
daekyha <gkeorud2252s@gmail.com>
2026-09-16 02:13:38 +0900

    merge

---
+++ koreaIT/src/com/koreaIT/JAM/dao/ArticleDao.java
@@ -0,0 +1,51 @@
+package com.koreaIT.JAM.dao;
+
+
+import java.util.ArrayList;
+import java.util.List;
+
+import com.koreaIT.JAM.dto.Article;
+import com.koreaIT.JAM.util.Util;
+
+public class ArticleDao {
+	private List<Article> articles;
+	private int articleId;
+	
+	public ArticleDao() {
+		this.articles = new ArrayList<Article>();
+		this.articleId = 0;
+	}
+
+	public int wrtieArticle(int id, String title, String body) {
+		articleId++;
+		articles.add(new Article (articleId, Util.getDateStr(), Util.getDateStr(), id, title, body));
+		return articleId;
+	}
+
+	public List<Article> getArticles() {
+		return articles;
+	}
+
+	public Article getArticleById(int id) {
+		for (Article article : articles) {
+			if (id == article.getId()) {
+				return article;
+			}
+		}
+		
+		return null;
+	}
+
+	public void modifyArticle(Article foundArticle, String title, String body) {
+		foundArticle.setTitle(title);
+		foundArticle.setBody(body);
+		foundArticle.setUpdateDate(Util.getDateStr());
+	}
+
+	public void deleteArticle(Article foundArticle) {
+		articles.remove(foundArticle);
+	}
+	
+
+
+}
